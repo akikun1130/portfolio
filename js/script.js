0f72d3d73c9c9df7424ebed2bb7de0fd41143df5
akikun1130 <57184632+akikun1130@users.noreply.github.com>
2020-01-17 12:52:23 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$ (function() {
+
+  $('.background').fadeIn(1500);
+
+  // clickイベントを記述したままだと他の操作も無効になる
+  // clickイベントをコメントアウトすると正常に動作
+
+  // $('a').click(fucntion() {
+  //   $(this).hide();
+  // });
+  // $('h1').css('color', 'red');
+
+});
